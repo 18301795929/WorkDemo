63d1db1c62d707ce5653b95291380c6f77e56d68
18301795929 <437547612@qq.com>
2017-05-10 16:45:00 +0800

    first commit

---
+++ WorkDemo/GCD/View/GCDView.h
@@ -0,0 +1,13 @@
+//
+//  GCDView.h
+//  WorkDemo
+//
+//  Created by Work(PWD:123) on 2017/5/9.
+//  Copyright © 2017年 Work(PWD:123). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GCDView : UIView
+
+@end
---
+++ WorkDemo/GCD/View/GCDView.m
@@ -0,0 +1,21 @@
+//
+//  GCDView.m
+//  WorkDemo
+//
+//  Created by Work(PWD:123) on 2017/5/9.
+//  Copyright © 2017年 Work(PWD:123). All rights reserved.
+//
+
+#import "GCDView.h"
+
+@implementation GCDView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
